245851e5dc6111ee4a42a8f56b2acf41e83376f5
Maheboob7699 <murdimaheboob@gmail.com>
2024-05-17 01:27:16 -0900

    objetc_task

---
+++ object_Task/index.js
@@ -0,0 +1,31 @@
+// Creating  an Array
+
+let product=[
+
+// bags
+ Bag={
+    Bag_Name: "Americian_tourister",
+    category:["school_bag","college_bag","ladies_bag","travelling_bag"],
+    Material:"Polyster",
+    Quantity:2,
+    size:["Large","Medium","small"],
+    country:"India",
+    Mrp:"rupees",
+    Currency:"$",
+    }
+ ,
+    
+  shirt={
+
+    shirt_Name:"AMIRI",
+    category:["ladies","Kids","Mens"],
+    pattern:"solid",
+    size:["Large","Medium","small"],
+    star_Rating:5.9,
+    
+    }
+
+
+]
+
+console.log(product);
