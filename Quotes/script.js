9504a8734b9bf80235ac45f70e415b479c36cc4a
Maheboob7699 <murdimaheboob@gmail.com>
2024-10-20 00:49:20 -0900

    Quotes

--- Quotes/script.js
+++ Quotes/script.js
@@ -1,63 +1,60 @@
-let quotes = [
-    { 
-        quote: "The only limit to our realization of tomorrow is our doubts of today.", 
-        author: "-Franklin D. Roosevelt" 
-    },
-
-    { 
-        quote: "Life is 10% what happens to us and 90% how we react to it", author: "-Charles R. Swindoll" 
-    },
-
-    { 
-        quote: "The purpose of our lives is to be happy.", 
-        author: "-Dalai Lama"
-     },
-
-    { 
-        quote: "Get busy living or get busy dying.", 
-        author: "-Stephen King"
-     },
-
-    { 
-        quote: "You have within you right now, everything you need to deal with whatever the world can throw at you.",
-         author: "-Brian Tracy" 
-    },
-
-    { 
-        quote: "Believe you can and you're halfway there.", 
-        author: "-Theodore Roosevelt" 
-    },
-
-    { 
-        quote: "The only way to do great work is to love what you do.", author: "-Steve Jobs"
-     },
-
-    { 
-        quote: "In the end, we only regret the chances we didn't take.", author: "-Lewis Carroll"
-     },
-
-    { 
-        quote: "Success is not the key to happiness. Happiness is the key to success.",
-         author: "-Albert Schweitzer" 
-    },
-
-    { quote: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
-         author: "-Ralph Waldo Emerson"
-     }
+const quotesArray = [
+  { quote: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" },
+  { quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.", author: "Martin Luther King Jr." },
+  { quote: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+  { quote: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer" },
+  { quote: "What we think, we become.", author: "Buddha" },
+  { quote: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+  { quote: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+  { quote: "You miss 100% of the shots you don’t take.", author: "Wayne Gretzky" },
+  { quote: "It is never too late to be what you might have been.", author: "George Eliot" },
+  { quote: "Act as if what you do makes a difference. It does.", author: "William James" }
 ];
 
-localStorage.setItem('quotes', JSON.stringify(quotes));
-let localQuotes = JSON.parse(localStorage.getItem('quotes'));
+//  let storeArray = localStorage.setItem('quotesData',JSON.stringify(quotesArray));
+
+// if (!localStorage.getItem('quotesData')) {
+//     localStorage.setItem('quotesData', JSON.stringify(quotesArray));
+// }
+
+let localArray = JSON.parse(localStorage.getItem('quotesData'));
 
 
-let generateBtn = document.querySelector(".generateBtn");
-let quotesBox = document.querySelector(".quotes");
-let authorBox = document.querySelector(".author");
-generateBtn.addEventListener("click",()=>generate())
+console.log(localArray.length);
 
-function generate(){
-  let index =  Math.floor(Math.random() * localQuotes.length);
-  quotesBox.innerHTML= quotes[index].quote;
-  authorBox.innerHTML = quotes[index].author;
-    
-}
+
+let generateBtn  = document.querySelector(".generateBtn");
+let quotesRender = document.querySelector(".quotesValue");
+let authorRender = document.querySelector(".authorValue");
+
+generateBtn.addEventListener("click",function(){
+let index = Math.floor(Math.random() * localArray.length);
+  console.log(index);
+  quotesRender.innerHTML = localArray[index].quote;
+  authorRender.innerHTML = localArray[index].author;
+  
+})
+
+
+
+
+
+let addQuotes = document.querySelector(".addQuotes");
+let quotesInput = document.querySelector("#quotesInput");
+let authorInput = document.querySelector("#authorInput");
+
+addQuotes.addEventListener("click",function(){
+  const newQuotes =  JSON.parse(localStorage.getItem('quotesData'));
+   newQuotes.push({
+       quote:quotesInput.value,
+      author:authorInput.value,
+})
+   console.log(newQuotes);
+  
+
+ localStorage.setItem('quotesData',JSON.stringify(newQuotes)); 
+quotesInput.value = '';
+authorInput.value = '';
+
+   })
+
